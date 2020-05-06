c700bff2fc8c4faf0d4eca03820a26c956c3d49b
Anders Busk <anbusk92@gmail.com>
2020-05-06 18:12:17 +0200

    To the limiut

--- ContainerizedPrimeCalculator/PrimeAPI/Program.cs
+++ ContainerizedPrimeCalculator/PrimeAPI/Program.cs
@@ -14,11 +14,10 @@ namespace PrimeAPI
     {
         public static void Main(string[] args)
         {
- 
             Log.Logger = new LoggerConfiguration()
             .WriteTo.Console()
             //.WriteTo.AzureAnalytics(workspaceId: "26ad584a-adb2-4c8a-9339-c9777999f169",
-                            //authenticationId: "hZlzRkQdSebh1lcIZP/wJGi5vR72XJIsFa5+FMNdB8cCx2zOtaVSccQ3JGIizQsNbQ5KJWCCmYxht77gvQwc8A==")
+            //authenticationId: "hZlzRkQdSebh1lcIZP/wJGi5vR72XJIsFa5+FMNdB8cCx2zOtaVSccQ3JGIizQsNbQ5KJWCCmYxht77gvQwc8A==")
             .CreateLogger();
             CreateHostBuilder(args).Build().Run();
 
